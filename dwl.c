6906c6f0494f3029c4cc92d4dd82780d8953f402
zhmlst <zhmlst.btw@gmail.com>
2025-06-26 05:23:02 +0300

    Revert "simpleborders"
    This reverts commit 6b21d030142e738f028bdbf962978a8d61143933.

--- dwl.c
+++ dwl.c
@@ -300,7 +300,6 @@ static void closemon(Monitor *m);
 static void commitlayersurfacenotify(struct wl_listener *listener, void *data);
 static void commitnotify(struct wl_listener *listener, void *data);
 static void commitpopup(struct wl_listener *listener, void *data);
-static int countclients(Monitor *m);
 static void createdecoration(struct wl_listener *listener, void *data);
 static void createidleinhibitor(struct wl_listener *listener, void *data);
 static void createkeyboard(struct wlr_keyboard *keyboard);
@@ -354,7 +353,6 @@ static void motionnotify(uint32_t time, struct wlr_input_device *device, double 
 		double sy, double sx_unaccel, double sy_unaccel);
 static void motionrelative(struct wl_listener *listener, void *data);
 static void moveresize(const Arg *arg);
-static int needsborder(Client *c);
 static void outputmgrapply(struct wl_listener *listener, void *data);
 static void outputmgrapplyortest(struct wlr_output_configuration_v1 *config, int test);
 static void outputmgrtest(struct wl_listener *listener, void *data);
@@ -1198,17 +1196,6 @@ commitpopup(struct wl_listener *listener, void *data)
 	wlr_xdg_popup_unconstrain_from_box(popup, &box);
 	wl_list_remove(&listener->link);
 	free(listener);
-}
-
-int
-countclients(Monitor *m)
-{
-    unsigned int n = 0;
-    Client *c;
-    wl_list_for_each(c, &clients, link)
-        if (VISIBLEON(c, m) && !c->isfloating && !c->isfullscreen)
-            n++;
-        return n;
 }
 
 void
@@ -2573,13 +2560,6 @@ moveresize(const Arg *arg)
 	}
 }
 
-int
-needsborder(Client *c) {
-	return ((countclients(c->mon) > 1)
-		|| c->isfloating)
-		&& !c->isfullscreen;
-}
-
 void
 outputmgrapply(struct wl_listener *listener, void *data)
 {
@@ -2777,7 +2757,6 @@ resize(Client *c, struct wlr_box geo, int interact)
 
 	client_set_bounds(c, geo.width, geo.height);
 	c->geom = geo;
-	c->bw = needsborder(c) ? borderpx : 0;
 	applybounds(c, bbox);
 
 	/* Update scene-graph, including borders */
